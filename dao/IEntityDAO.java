ad5076d55f4d79188ee66967714fb63f6f6f9451
Davi Luna <Sdavi738@gmail.com>
2023-06-09 23:17:11 -0300

    feat: Adicionando as primeiras features do BD de acordo com o modelo de projeto DAO

---
+++ dao/IEntityDAO.java
@@ -0,0 +1,15 @@
+/*De acordo com o padrão de projeto DAO essa classe vai especificar os métodos
+que deverão ser obrigatoriamente implementados
+portanto serão as operações CRUD do BD.
+ */
+
+import java.util.List;
+
+public interface IEntityDAO <T>{
+
+	public void save(T t);
+	public T findById(Integer id);
+	public List<T> findAll();
+	public void update(T t);
+	public void delete(T t);
+}
