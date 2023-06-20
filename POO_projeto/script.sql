4e151c4beaed1283d875dd739d4939569a427d16
Davi Souza de Luna <Sdavi738@gmail.com>
2023-06-20 15:26:33 -0300

    Adicionando o novo script do BD

--- POO_projeto/script.sql
+++ POO_projeto/script.sql
@@ -2,18 +2,47 @@ create database banco;
 use banco;
 
 
-create table conta(
+
+create table cliente(
 id int not null auto_increment,
-numero_conta int not null,
-saldo FLOAT(7,2),/*armazena sete algarismos, nos quais dois sao decimais*/
-status boolean,
+cpf varchar(30),
 primary key(id)
 );
 
-create table registro_transacao(
+create table registro_conta(
 id int not null auto_increment,
-numero_conta int not null,
-tipo varchar(50),
-data_hora_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+cpf varchar(30),
+numero_conta varchar(50) not null,
+saldo FLOAT(7,2),/*armazena sete algarismos, nos quais dois sao decimais*/
+tipo varchar(20),
+status boolean,
+data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+primary key(id)
 );
+
+create table conta_corrente(
+id int not null,
+numero_conta VARCHAR(50) not NULL,
+saldo FLOAT(7,2),
+status boolean,
+data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+primary key(id)
+);
+
+create table conta_poupanca(
+id int not null,
+numero_conta VARCHAR(50) not NULL,
+saldo FLOAT(7,2),
+status boolean,
+data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+primary key(id)
+);
+
+CREATE TABLE registro_transacao(
+id int not null,
+valor float,
+data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+primary key(id)
+);
+
+
